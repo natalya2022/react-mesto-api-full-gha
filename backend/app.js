82cefaa33fa242657dfc4348b5e0fc2d4e1a3f8e
Natalya Dmitrieva <buzlk.n@yandex.ru>
2023-08-10 17:53:42 +0400

    ПР15 деплой

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
+
 app.use(cors({ origin: ['http://localhost:3001', 'https://places.nomoreparties.co'], credentials: true }));
 
 const limiter = rateLimit({
