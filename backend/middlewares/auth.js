a7c74ad725f4e6de90d3fddd55fdcd0737fbfa87
Natalya Dmitrieva <buzlk.n@yandex.ru>
2023-08-10 21:15:50 +0400

    ПР15 деплой

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,13 +4,13 @@ const UnauthorizedError = require('../errors/unauthorized-err');
 
 const checkAuth = (req, res, next) => {
   if (!req.cookies) {
-    throw new UnauthorizedError('Ошибка авторизации. Нет cookie');
+    throw new UnauthorizedError('Ошибка авторизации.');
   }
   const token = req.cookies.jwt;
   const result = checkToken(token);
   req.user = result;
   if (!result) {
-    throw new UnauthorizedError(`Ошибка авторизации. Нет юзера ${token}`);
+    throw new UnauthorizedError(`Ошибка авторизации. Не передан токен. Значение ${token}`);
   }
   next();
 };
